38f02484f9b4d0bf4620a2b200bc8f6a4edc6615
Vitro66 <v.maneiro.r@gmail.com>
2024-09-20 12:12:27 +0200

    Add files via upload

---
+++ src/main/java/com/example/EjercicioIntegrador/EjercicioIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIntegradorApplication.class, args);
+	}
+
+}
